93de3dfd8ebe39bf00d1a6fc394fc541f3f119af
venkadesh004 <venkadeshdeveloper@gmail.com>
2024-02-24 19:55:57 +0530

    fix screen

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://localhost:3002",
+  baseURL: "https://techutsav-admin-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
